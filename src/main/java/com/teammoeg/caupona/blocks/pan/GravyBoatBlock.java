e7237e44d4ee6118e4110f639872be763a9e9adf
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2022-07-31 13:52:48 +0800

    fix creative selection 2

--- src/main/java/com/teammoeg/caupona/blocks/pan/GravyBoatBlock.java
+++ src/main/java/com/teammoeg/caupona/blocks/pan/GravyBoatBlock.java
@@ -22,19 +22,23 @@ import java.util.List;
 
 import com.teammoeg.caupona.CPItems;
 import com.teammoeg.caupona.blocks.CPHorizontalBlock;
+import com.teammoeg.caupona.blocks.foods.BowlTileEntity;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction.Axis;
 import net.minecraft.core.NonNullList;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition.Builder;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.api.distmarker.Dist;
@@ -113,4 +117,10 @@ public class GravyBoatBlock extends CPHorizontalBlock {
 		return sep;
 		
 	}
+	public ItemStack getCloneItemStack(BlockState state, HitResult target, BlockGetter level, BlockPos pos, Player player)
+    {
+		ItemStack is=new ItemStack(CPItems.gravy_boat);
+		is.setDamageValue(state.getValue(LEVEL));
+		return is;
+    }
 }
