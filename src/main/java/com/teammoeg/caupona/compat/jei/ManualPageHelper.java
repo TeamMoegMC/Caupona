5766b110b78eb4e1f6f535ba3f9716fe0bb42180
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2023-03-31 16:35:36 +0800

    Upload

--- src/main/java/com/teammoeg/caupona/compat/jei/ManualPageHelper.java
+++ src/main/java/com/teammoeg/caupona/compat/jei/ManualPageHelper.java
@@ -4,5 +4,5 @@ import mezz.jei.common.gui.elements.DrawableNineSliceTexture;
 import net.minecraft.resources.ResourceLocation;
 
 public class ManualPageHelper {
-	DrawableNineSliceResource t=new DrawableNineSliceResource(new ResourceLocation(), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+	//DrawableNineSliceResource t=new DrawableNineSliceResource(new ResourceLocation(), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 }
